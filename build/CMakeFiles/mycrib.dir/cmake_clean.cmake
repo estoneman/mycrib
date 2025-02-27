640bde80692590dac2c0e3bd934f81f8f5de4957
estoneman <elstoneman11@gmail.com>
2025-02-27 00:29:57 -0600

    initial commit

---
+++ build/CMakeFiles/mycrib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mycrib.dir/mycrib.c.o"
+  "CMakeFiles/mycrib.dir/mycrib.c.o.d"
+  "mycrib"
+  "mycrib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mycrib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
