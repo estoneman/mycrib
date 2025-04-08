68f7dd1ae437d023c1586fd4ba79bc17f2cd2f48
estoneman <elstoneman11@gmail.com>
2025-04-07 19:42:25 -0500

    modularize and refactor

--- src/mycrib.c
+++ src/mycrib.c
@@ -20,15 +20,13 @@ size_t n_routes = ARRAY_LEN (ROUTES);
 int
 valid_route (const char *req_url)
 {
-  int found = -1;
-
   for (size_t i = 0; i < n_routes; ++i)
     {
       if (strcmp (ROUTES[i], req_url) == 0)
-        found = 0;
+        return 0;
     }
 
-  return found;
+  return -1;
 }
 
 void
@@ -39,13 +37,42 @@ mem_free (void *mem)
 }
 
 enum MHD_Result
+send_response (struct MHD_Connection *connection, const char *buf,
+               size_t bufsize, int code)
+{
+  struct MHD_Response *response;
+  int ret;
+
+  response = MHD_create_response_from_buffer (bufsize, (void *)buf,
+                                              MHD_RESPMEM_PERSISTENT);
+  ret = MHD_queue_response (connection, code, response);
+  MHD_destroy_response (response);
+
+  return ret;
+}
+
+enum MHD_Result
+send_response_free_callback (struct MHD_Connection *connection,
+                             const char *buf, size_t bufsize, int code,
+                             MHD_ContentReaderFreeCallback crfc)
+{
+  struct MHD_Response *response;
+  int ret;
+
+  response = MHD_create_response_from_buffer_with_free_callback (
+      bufsize, (void *)buf, crfc);
+  ret = MHD_queue_response (connection, code, response);
+  MHD_destroy_response (response);
+
+  return ret;
+}
+
+enum MHD_Result
 router (struct MHD_Connection *connection, const char *url)
 {
   char *buf;
   size_t urllen, buflen;
-  int ret;
   const char *page;
-  struct MHD_Response *response;
 
   urllen = strlen (url);
   buflen = 1024 + (urllen > MAX_ROUTE_LEN ? MAX_ROUTE_LEN : urllen);
@@ -54,12 +81,8 @@ router (struct MHD_Connection *connection, const char *url)
     {
       page = "<html><body>Internal Server Error</body></html>";
 
-      response = MHD_create_response_from_buffer (strlen (page), (void *)page,
-                                                  MHD_RESPMEM_PERSISTENT);
-      ret = MHD_queue_response (connection, MHD_HTTP_INTERNAL_SERVER_ERROR,
-                                response);
-
-      return ret;
+      return send_response (connection, page, strlen (page),
+                            MHD_HTTP_INTERNAL_SERVER_ERROR);
     }
 
   if (valid_route (url) == -1)
@@ -67,24 +90,15 @@ router (struct MHD_Connection *connection, const char *url)
       page = "<html><body>Invalid route: %s</body></html>";
       snprintf (buf, buflen, page, url);
 
-      response = MHD_create_response_from_buffer_with_free_callback (
-          strlen (buf), (void *)buf, &mem_free);
-      ret = MHD_queue_response (connection, MHD_HTTP_NOT_FOUND, response);
-
-      return ret;
+      return send_response_free_callback (connection, buf, strlen (buf),
+                                          MHD_HTTP_NOT_FOUND, &mem_free);
     }
 
   page = "<html><body>%s</body></html>";
   snprintf (buf, buflen, page, url);
 
-  response = MHD_create_response_from_buffer (strlen (buf), (void *)buf,
-                                              MHD_RESPMEM_PERSISTENT);
-  ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
-
-  MHD_destroy_response (response);
-  free (buf);
-
-  return ret;
+  return send_response_free_callback (connection, buf, strlen (buf),
+                                      MHD_HTTP_OK, &mem_free);
 }
 
 enum MHD_Result
@@ -110,23 +124,14 @@ answer_connection (void *cls, struct MHD_Connection *connection,
   (void)cls;
   (void)req_cls;
 
-  struct MHD_Response *response;
-  const char *page;
-  int ret;
-
   if (dh_check_method (method) == MHD_NO)
     {
-      page = "<html></body>Method not allowed</body></html>\n";
-      response = MHD_create_response_from_buffer (strlen (page), (void *)page,
-                                                  MHD_RESPMEM_PERSISTENT);
-      ret = MHD_queue_response (connection, MHD_HTTP_METHOD_NOT_ALLOWED,
-                                response);
-      MHD_destroy_response (response);
+      const char *page;
+      page = "<html><body>Method not allowed</body></html>\n";
 
-      return ret;
+      return send_response (connection, page, strlen (page),
+                            MHD_HTTP_METHOD_NOT_ALLOWED);
     }
-
-  page = "<html><body>Hello, Sailor!</body></html>\n";
 
   return router (connection, url);
 }
@@ -136,11 +141,10 @@ main (void)
 {
   struct MHD_Daemon *daemon;
   enum MHD_FLAG flags = MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_DEBUG
-                        | MHD_USE_THREAD_PER_CONNECTION
                         | MHD_USE_PEDANTIC_CHECKS;
 
   daemon = MHD_start_daemon (flags, PORT, NULL, NULL, &answer_connection,
-                             (void *)&dh_check_method, MHD_OPTION_END);
+                             NULL, MHD_OPTION_END);
 
   if (NULL == daemon)
     {
