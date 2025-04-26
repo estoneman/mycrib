ca27c81b4969dc6c7a6fb84d9ca69d91d49ee4c3
Ethan Stoneman <elstoneman11@gmail.com>
2025-04-26 01:09:10 -0500

    add support for TLS

--- src/mycrib.c
+++ src/mycrib.c
@@ -1,21 +1,63 @@
-/* SUPPORTED API ROUTES
- *   - Home page: GET /
- *   - Get all movies: GET /movies
- *   - Get movie: GET /movie?title=<title>&search_type=<type>
- */
-
 #include <assert.h>
 #include <microhttpd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #include "route.h"
 
-#define PORT 8888
+#define PORT 8080
 #define ARRAY_LEN(arr) (sizeof (arr) / sizeof ((arr)[0]))
 #define MAX_ROUTE_LEN 64
 #define ROUTE_TABLE_SIZE (2 << 15)
+
+#define SERVERKEYFILE "../pki/mycrib.key"
+#define SERVERCERTFILE "/etc/pki/ca-trust/source/anchors/mycrib.pem"
+
+static long
+get_file_size (const char *filename)
+{
+  struct stat stat_buf;
+
+  if (-1 == stat (filename, &stat_buf))
+    return 0;
+
+  return (long)stat_buf.st_size;
+}
+
+static char *
+load_file (const char *filename)
+{
+  FILE *fp;
+  char *buffer;
+  long size;
+
+  size = get_file_size (filename);
+  if (0 == size)
+    return NULL;
+
+  fp = fopen (filename, "rb");
+  if (!fp)
+    return NULL;
+
+  buffer = malloc (size + 1);
+  if (!buffer)
+    {
+      fclose (fp);
+      return NULL;
+    }
+  buffer[size] = '\0';
+
+  if (size != (long)fread (buffer, 1, size, fp))
+    {
+      free (buffer);
+      buffer = NULL;
+    }
+
+  fclose (fp);
+  return buffer;
+}
 
 void
 mem_free (void *mem)
@@ -63,11 +105,16 @@ enum MHD_Result
 router (Route *routes, struct MHD_Connection *connection, const char *url)
 {
   const char *page;
-  char *error, *response;
+  char *error, *response, san_url[MAX_ROUTE_LEN + 1];
   size_t url_len, error_len;
 
   url_len = strlen (url);
   error_len = 1024 + (url_len > MAX_ROUTE_LEN ? MAX_ROUTE_LEN : url_len);
+  strncpy (san_url, url, MAX_ROUTE_LEN);
+  san_url[MAX_ROUTE_LEN - 3] = '.';
+  san_url[MAX_ROUTE_LEN - 2] = '.';
+  san_url[MAX_ROUTE_LEN - 1] = '.';
+  san_url[MAX_ROUTE_LEN] = '\0';
 
   if ((error = malloc (error_len + 1)) == NULL)
     {
@@ -77,13 +124,13 @@ router (Route *routes, struct MHD_Connection *connection, const char *url)
                             MHD_HTTP_INTERNAL_SERVER_ERROR);
     }
 
-  unsigned id = small_crc16_8005 (url, url_len);
+  unsigned id = small_crc16_8005 (san_url, url_len);
 
   if (!routes[id].path)
     {
       page = "{\"error\": \"%s '%s'\"}";
       snprintf (error, error_len, page,
-                MHD_get_reason_phrase_for (MHD_HTTP_NOT_FOUND), url);
+                MHD_get_reason_phrase_for (MHD_HTTP_NOT_FOUND), san_url);
 
       return send_response_free_callback (connection, error, strlen (error),
                                           MHD_HTTP_NOT_FOUND, &mem_free);
@@ -143,11 +190,18 @@ int
 main (void)
 {
   struct MHD_Daemon *daemon;
-  enum MHD_FLAG flags = MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_DEBUG
-                        | MHD_USE_PEDANTIC_CHECKS | MHD_USE_TCP_FASTOPEN
-                        | MHD_USE_ITC | MHD_USE_THREAD_PER_CONNECTION;
-
+  enum MHD_FLAG flags;
   Route *routes;
+  char *key_pem, *cert_pem;
+
+  cert_pem = load_file (SERVERCERTFILE);
+  key_pem = load_file (SERVERKEYFILE);
+
+  if ((key_pem == NULL) || (cert_pem == NULL))
+    {
+      printf ("the key/certificate files could not be read.\n");
+      return EXIT_FAILURE;
+    }
 
   if ((routes = (Route *)calloc (ROUTE_TABLE_SIZE, sizeof (Route))) == NULL)
     {
@@ -159,11 +213,21 @@ main (void)
   new_route (routes, "/movie", movie_handler);
   new_route (routes, "/movies", movies_handler);
 
-  daemon = MHD_start_daemon (flags, PORT, NULL, NULL, &answer_connection,
-                             &routes, MHD_OPTION_END);
+  flags = MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_THREAD_PER_CONNECTION
+          | MHD_USE_ITC | MHD_USE_TCP_FASTOPEN | MHD_USE_TLS
+          | MHD_USE_PEDANTIC_CHECKS | MHD_USE_DEBUG;
+  daemon
+      = MHD_start_daemon (flags, PORT, NULL, NULL, &answer_connection, &routes,
+                          MHD_OPTION_HTTPS_MEM_KEY, key_pem,
+                          MHD_OPTION_HTTPS_MEM_CERT, cert_pem, MHD_OPTION_END);
 
   if (NULL == daemon)
     {
+      printf ("%s\n", cert_pem);
+
+      free (key_pem);
+      free (cert_pem);
+
       return EXIT_FAILURE;
     }
 
@@ -175,5 +239,10 @@ main (void)
   del_route (routes, "/movie");
   del_route (routes, "/movies");
 
+  free (key_pem);
+  free (cert_pem);
+
   return EXIT_SUCCESS;
 }
+
+// vim: sts=2 ts=2 et ai tw=80
