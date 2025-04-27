66169e8d07a1d57654abc0274c6276dd3904d9d3
Ethan Stoneman <elstoneman11@gmail.com>
2025-04-27 14:15:16 -0500

    add cert creation script

---
+++ scripts/mk_cert.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+declare -i _BITS _DAYS
+
+_PROJECT_HOME="$HOME/Programming/projects/mycrib"
+_OUT="$_PROJECT_HOME/pki"
+
+_gen_key ()
+{
+    # $1 => number of bits
+
+    printf 'Generating RSA key, bits=%d... ' "$1"
+    openssl genrsa -out "$_OUT/mycrib.key"
+    echo 'Done'
+}
+
+_gen_ss_cert ()
+{
+    # $1 => number of days
+
+    printf 'Generating self-signed certificate... '
+    openssl req -days "$1" -out mycrib.pem -new -x509 -key "$_OUT/mycrib.key"
+    echo 'Done'
+}
+
+_BITS=4096
+if [ -f "$_OUT/mycrib.key" ]; then
+    read -p 'Key exists, overwrite (y/n)? ' -r -n 1 _PERM; echo
+    [ "${_PERM@L}" = 'y' ] && _gen_key $_BITS
+fi
+
+_DAYS=365
+if [ -f "$_OUT/mycrib.pem" ]; then
+    read -p 'Certificate exists, overwrite (y/n)? ' -r -n 1 _PERM; echo
+    [ "${_PERM@L}" = 'y' ] && _gen_ss_cert $_DAYS
+fi
+
+unset _BITS _OUT _PERM _PROJECT_HOME
