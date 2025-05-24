e91ebffe5521139cd92cf46592edb9bd72129fda
Ethan Stoneman <elstoneman11@gmail.com>
2025-05-24 12:46:36 -0500

    forgot to add previous documentation

--- include/handler.h
+++ include/handler.h
@@ -4,7 +4,52 @@
 #include <microhttpd.h>
 
 char *root_handler(struct MHD_Connection *);
+
+/**
+ * Returns the record of a movie, if it exists
+ *
+ * GET /movie?title=<title>
+ *
+ * @param connection MHD connection that stores the title to be searched
+ * @return JSON-formatted response, whether the operation succeeded or not
+ *
+ * {
+ *     "status": 200,
+ *     "result": {
+ *       "cast"         : "cast",
+ *       "director"     : "director",
+ *       "duration"     : "duration",
+ *       "genre"        : "genre",
+ *       "poster_url"   : "poster_url",
+ *       "rating_family": "rating_family",
+ *       "rating_imdb"  : "rating_imdb",
+ *       "title":       : "title",
+ *       "year"         : "year"
+ *     }
+ *  }
+ *
+ *  {
+ *     "status": 404,
+ *     "result": {
+ *       "error": "%s not found"
+ *     }
+ *  }
+ *
+ *  {
+ *     "status": 400
+ *     "result": {
+ *       "error": "title not provided"
+ *     }
+ *  }
+ *
+ *  {
+ *    "status": 500,
+ *    "result": {
+ *      "error": "<error message>"
+ *    }
+ */
 char *movie_handler(struct MHD_Connection *);
+
 char *movies_handler(struct MHD_Connection *);
 
 #endif  // _HANDLER_H
