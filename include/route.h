db17366743ceaeb93a2c374c93225413ea5cce54
Ethan Stoneman <elstoneman11@gmail.com>
2025-06-08 23:23:56 -0500

    abstract function signature of handlers

--- include/route.h
+++ include/route.h
@@ -4,15 +4,24 @@
 #include <jansson.h>
 #include <microhttpd.h>
 
+typedef union {
+  json_t *(*handler_arg)(struct MHD_Connection *, const char *);
+  json_t *(*handler_void)(void);
+} Handler;
+
+typedef enum { HANDLER_ARG, HANDLER_VOID } HandlerType;
+
 typedef struct {
   char *path;
-  json_t *(*handler)(struct MHD_Connection *);
+  HandlerType type;
+  Handler handler;
 } Route;
 
-void new_route(Route *routes, const char *path,
-               json_t *(*handler)(struct MHD_Connection *));
+uint16_t small_crc16_8005(const char *m, size_t n);
+void new_route(Route *routes, const char *path, HandlerType type,
+               Handler handler);
 void del_route(Route *routes, const char *path);
 enum MHD_Result router(Route *routes, struct MHD_Connection *connection,
-                       const char *url);
+                       const char *method, const char *url);
 
 #endif  // _ROUTE_H
