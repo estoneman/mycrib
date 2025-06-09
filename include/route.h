116f580d0337fbff8fc582c0bb89af60d162c186
Ethan Stoneman <elstoneman11@gmail.com>
2025-06-08 19:41:59 -0500

    re-type handlers

--- include/route.h
+++ include/route.h
@@ -1,15 +1,16 @@
 #ifndef _ROUTE_H
 #define _ROUTE_H
 
+#include <jansson.h>
 #include <microhttpd.h>
 
 typedef struct {
   char *path;
-  char *(*handler)(struct MHD_Connection *);
+  json_t *(*handler)(struct MHD_Connection *);
 } Route;
 
 uint16_t small_crc16_8005(const char *, size_t);
-void new_route(Route *, const char *, char *(*)(struct MHD_Connection *));
+void new_route(Route *, const char *, json_t *(*)(struct MHD_Connection *));
 void del_route(Route *, const char *);
 
 #endif  // _ROUTE_H
