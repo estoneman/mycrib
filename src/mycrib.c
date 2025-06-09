7915e49a33853d23e2e9deb7b27310bad342207f
Ethan Stoneman <elstoneman11@gmail.com>
2025-06-08 19:40:17 -0500

    delete '/movie' endpoint and patternize error message building

--- src/mycrib.c
+++ src/mycrib.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <jansson.h>
 #include <microhttpd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,6 +13,7 @@
 #define ARRAY_LEN(arr) (sizeof(arr) / sizeof((arr)[0]))
 #define MAX_ROUTE_LEN 64
 #define ROUTE_TABLE_SIZE (2 << 15)
+#define ERR_TEMPLATE "{\"status\": %i, \"result\": \"%s\"}"
 
 #define SERVERKEYFILE "../pki/mycrib.key"
 #define SERVERCERTFILE "../pki/mycrib.pem"
@@ -96,40 +98,72 @@ enum MHD_Result send_response_free_callback(
 
 enum MHD_Result router(Route *routes, struct MHD_Connection *connection,
                        const char *url) {
-  const char *page;
-  char *error, *response, san_url[MAX_ROUTE_LEN + 1];
-  size_t url_len, error_len;
+  char *response;
+  size_t url_len;
+  json_t *result, *status_json;
+  int status;
+  unsigned id;
+
+  char *error;
+  int required_len;
 
   url_len = strlen(url);
-  error_len = 1024 + (url_len > MAX_ROUTE_LEN ? MAX_ROUTE_LEN : url_len);
-  strncpy(san_url, url, MAX_ROUTE_LEN);
-  san_url[MAX_ROUTE_LEN - 3] = '.';
-  san_url[MAX_ROUTE_LEN - 2] = '.';
-  san_url[MAX_ROUTE_LEN - 1] = '.';
-  san_url[MAX_ROUTE_LEN] = '\0';
+  if (url_len > MAX_ROUTE_LEN) {
+    required_len = snprintf(NULL, 0, ERR_TEMPLATE, MHD_HTTP_BAD_REQUEST,
+                            MHD_get_reason_phrase_for(MHD_HTTP_BAD_REQUEST));
+    if ((error = malloc(required_len + 1)) == NULL) {
+      return send_response(
+          connection, MHD_get_reason_phrase_for(MHD_HTTP_INTERNAL_SERVER_ERROR),
+          MHD_get_reason_phrase_len_for(MHD_HTTP_INTERNAL_SERVER_ERROR),
+          MHD_HTTP_INTERNAL_SERVER_ERROR);
+    }
+    snprintf(error, required_len + 1, ERR_TEMPLATE, MHD_HTTP_BAD_REQUEST,
+             MHD_get_reason_phrase_for(MHD_HTTP_BAD_REQUEST));
 
-  if ((error = malloc(error_len + 1)) == NULL) {
-    page = "{\"error\": \"Internal Server Error\"}";
-
-    return send_response(connection, page, strlen(page),
-                         MHD_HTTP_INTERNAL_SERVER_ERROR);
+    return send_response_free_callback(connection, error, required_len,
+                                       MHD_HTTP_BAD_REQUEST, &mem_free);
   }
 
-  unsigned id = small_crc16_8005(san_url, url_len);
+  id = small_crc16_8005(url, url_len);
 
   if (!routes[id].path) {
-    page = "{\"error\": \"%s '%s'\"}";
-    snprintf(error, error_len, page,
-             MHD_get_reason_phrase_for(MHD_HTTP_NOT_FOUND), san_url);
+    required_len = snprintf(NULL, 0, ERR_TEMPLATE, MHD_HTTP_NOT_FOUND,
+                            MHD_get_reason_phrase_for(MHD_HTTP_NOT_FOUND));
+    if ((error = malloc(required_len + 1)) == NULL) {
+      return send_response(
+          connection, MHD_get_reason_phrase_for(MHD_HTTP_INTERNAL_SERVER_ERROR),
+          MHD_get_reason_phrase_len_for(MHD_HTTP_INTERNAL_SERVER_ERROR),
+          MHD_HTTP_INTERNAL_SERVER_ERROR);
+    }
+    snprintf(error, required_len + 1, ERR_TEMPLATE, MHD_HTTP_NOT_FOUND,
+             MHD_get_reason_phrase_for(MHD_HTTP_NOT_FOUND));
 
-    return send_response_free_callback(connection, error, strlen(error),
+    return send_response_free_callback(connection, error, required_len,
                                        MHD_HTTP_NOT_FOUND, &mem_free);
   }
 
-  response = routes[id].handler(connection);
+  result = routes[id].handler(connection);
+
+  response = json_dumps(result, 0);
+
+  status_json = json_object_get(result, "status");
+  if (!status_json)
+    return send_response(
+        connection, MHD_get_reason_phrase_for(MHD_HTTP_INTERNAL_SERVER_ERROR),
+        MHD_get_reason_phrase_len_for(MHD_HTTP_INTERNAL_SERVER_ERROR),
+        MHD_HTTP_INTERNAL_SERVER_ERROR);
+
+  status = json_integer_value(status_json);
+  if (status == 0)
+    return send_response(
+        connection, MHD_get_reason_phrase_for(MHD_HTTP_INTERNAL_SERVER_ERROR),
+        MHD_get_reason_phrase_len_for(MHD_HTTP_INTERNAL_SERVER_ERROR),
+        MHD_HTTP_INTERNAL_SERVER_ERROR);
+
+  json_decref(result);
 
   return send_response_free_callback(connection, response, strlen(response),
-                                     MHD_HTTP_OK, &mem_free);
+                                     status, &mem_free);
 }
 
 enum MHD_Result dh_check_method(const char *method) {
@@ -152,12 +186,23 @@ enum MHD_Result answer_connection(void *cls, struct MHD_Connection *connection,
 
   static int ctx;
 
+  int required_len;
+  char *error;
   if (dh_check_method(method) == MHD_NO) {
-    const char *page;
-    page = "{\"error\": \"Method not allowed\"}";
+    required_len =
+        snprintf(NULL, 0, ERR_TEMPLATE, MHD_HTTP_METHOD_NOT_ALLOWED,
+                 MHD_get_reason_phrase_for(MHD_HTTP_METHOD_NOT_ALLOWED));
+    if ((error = malloc(required_len + 1)) == NULL) {
+      return send_response(
+          connection, MHD_get_reason_phrase_for(MHD_HTTP_INTERNAL_SERVER_ERROR),
+          MHD_get_reason_phrase_len_for(MHD_HTTP_INTERNAL_SERVER_ERROR),
+          MHD_HTTP_INTERNAL_SERVER_ERROR);
+    }
+    snprintf(error, required_len + 1, ERR_TEMPLATE, MHD_HTTP_METHOD_NOT_ALLOWED,
+             MHD_get_reason_phrase_for(MHD_HTTP_METHOD_NOT_ALLOWED));
 
-    return send_response(connection, page, strlen(page),
-                         MHD_HTTP_METHOD_NOT_ALLOWED);
+    return send_response_free_callback(connection, error, required_len,
+                                       MHD_HTTP_METHOD_NOT_ALLOWED, &mem_free);
   }
 
   if (&ctx != *req_cls) {
@@ -190,7 +235,6 @@ int main(void) {
   }
 
   new_route(routes, "/", root_handler);
-  new_route(routes, "/movie", movie_handler);
   new_route(routes, "/movies", movies_handler);
 
   flags = MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_THREAD_PER_CONNECTION |
@@ -213,11 +257,11 @@ int main(void) {
   MHD_stop_daemon(daemon);
 
   del_route(routes, "/");
-  del_route(routes, "/movie");
   del_route(routes, "/movies");
 
-  free(key_pem);
-  free(cert_pem);
+  if (routes) free(routes);
+  if (key_pem) free(key_pem);
+  if (cert_pem) free(cert_pem);
 
   return EXIT_SUCCESS;
 }
