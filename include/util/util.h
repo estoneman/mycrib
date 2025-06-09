5ae0d0e26b3246170d1dda46b7a312d8a3f58366
Ethan Stoneman <elstoneman11@gmail.com>
2025-06-08 22:25:56 -0500

    add hash function to util

--- include/util/util.h
+++ include/util/util.h
@@ -1,7 +1,11 @@
 #ifndef _UTIL_H
 #define _UTIL_H
 
+#include <stdint.h>
+#include <stdlib.h>
+
 long get_file_size(const char *filename);
 char *load_file(const char *filename);
+uint16_t small_crc16_8005(const char *m, size_t n);
 
 #endif  // _UTIL_H
