3e626beb79b84e5fe224e7b7bbda790fc03f603e
Ethan Stoneman <elstoneman11@gmail.com>
2025-06-11 20:51:13 -0500

    add code that creates query for GET and refactor

--- src/db.c
+++ src/db.c
@@ -8,6 +8,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define HTTP_OK_CODE 200
+#define HTTP_OK_MSG "OK"
+#define HTTP_BAD_REQUEST_CODE 400
+#define HTTP_BAD_REQUEST_MSG "Bad Request"
+#define HTTP_INTERNAL_SERVER_ERROR_CODE 500
+#define HTTP_INTERNAL_SERVER_ERROR_MSG "Internal Server Error"
+
+#define JSON_RESULT_FMT "{s:i,s:O}"
+#define JSON_ERROR_FMT "{s:i,s:s}"
+
 int build_stmt_va(sqlite3_stmt **pp_stmt, sqlite3 *db, const char *query,
                   size_t n, ...) {
   int rc;
@@ -39,7 +49,7 @@ int build_stmt_va(sqlite3_stmt **pp_stmt, sqlite3 *db, const char *query,
 
 sqlite3 *db_connect(void) {
   sqlite3 *db;
-  int rc;
+  int rc, old_val;
   const char *db_file = "../sql/mycrib.db";
 
   rc = sqlite3_open(db_file, &db);
@@ -49,8 +59,6 @@ sqlite3 *db_connect(void) {
 
     return NULL;
   }
-
-  int old_val;
 
   if ((rc = sqlite3_db_config(db, SQLITE_DBCONFIG_DEFENSIVE, 1, &old_val)) !=
       SQLITE_OK) {
@@ -75,7 +83,84 @@ int db_close(sqlite3 *db) {
   return 0;
 }
 
-json_t *db_exec_movie(sqlite3 *db, sqlite3_stmt *pp_stmt) {
+json_t *db_read_movies(const char *search_pattern, const char *search_type) {
+  size_t len_search_pattern;
+  size_t len_search_pattern_like;
+  char *search_pattern_like;
+  int rc;
+  json_t *record_set, *result;
+  const char *sql;
+  sqlite3 *db;
+  sqlite3_stmt *pp_stmt;
+
+  if (!(db = db_connect())) return NULL;
+
+  len_search_pattern = strlen(search_pattern);
+
+  if (strncmp(search_type, "contains", strlen("contains")) == 0) {
+    len_search_pattern_like = len_search_pattern + 2;  // 2 '%'
+
+    if (!(search_pattern_like = malloc(len_search_pattern_like + 1)))
+      return NULL;
+
+    snprintf(search_pattern_like, len_search_pattern_like + 1, "%%%s%%",
+             search_pattern);
+
+    sql = "SELECT * FROM movies WHERE title LIKE ?;";
+    if (build_stmt_va(&pp_stmt, db, sql, 1, search_pattern_like) == -1)
+      return NULL;
+
+    free(search_pattern_like);
+  } else if (strncmp(search_type, "endswith", strlen("endswith")) == 0) {
+    len_search_pattern_like = len_search_pattern + 1;  // 1 '%'
+
+    if (!(search_pattern_like = malloc(len_search_pattern_like + 1)))
+      return NULL;
+
+    snprintf(search_pattern_like, len_search_pattern_like + 1, "%%%s",
+             search_pattern);
+
+    sql = "SELECT * FROM movies WHERE title LIKE ?;";
+    if (build_stmt_va(&pp_stmt, db, sql, 1, search_pattern_like) == -1)
+      return NULL;
+
+    free(search_pattern_like);
+  } else if (strncmp(search_type, "startswith", strlen("startswith")) == 0) {
+    len_search_pattern_like = len_search_pattern + 1;  // 1 '%'
+
+    if (!(search_pattern_like = malloc(len_search_pattern_like + 1)))
+      return NULL;
+
+    snprintf(search_pattern_like, len_search_pattern_like + 1, "%s%%",
+             search_pattern);
+
+    sql = "SELECT * FROM movies WHERE title LIKE ?;";
+    if (build_stmt_va(&pp_stmt, db, sql, 1, search_pattern_like) == -1)
+      return NULL;
+
+    free(search_pattern_like);
+  } else if (strncmp(search_type, "exact", strlen("exact")) == 0) {
+    sql = "SELECT * FROM movies WHERE title = ?;";
+    if (build_stmt_va(&pp_stmt, db, sql, 1, search_pattern) == -1) return NULL;
+  } else {
+    return json_pack(JSON_ERROR_FMT, "status", HTTP_BAD_REQUEST_CODE, "error",
+                     "Unsupported search type");
+  }
+
+  record_set = db_exec_read_movies(db, pp_stmt);
+
+  if (!record_set) return NULL;
+
+  result =
+      json_pack(JSON_RESULT_FMT, "status", HTTP_OK_CODE, "result", record_set);
+  json_decref(record_set);
+
+  if ((rc = db_close(db)) != 0) return NULL;
+
+  return result;
+}
+
+json_t *db_exec_read_movies(sqlite3 *db, sqlite3_stmt *pp_stmt) {
   int rc;
 
   json_t *record_set = json_array();
@@ -93,42 +178,6 @@ json_t *db_exec_movie(sqlite3 *db, sqlite3_stmt *pp_stmt) {
                        sqlite3_column_text(pp_stmt, 8), "rating_imdb",
                        sqlite3_column_double(pp_stmt, 9));
     if (!record || json_array_append_new(record_set, record)) continue;
-  }
-
-  if (rc == SQLITE_ERROR || rc == SQLITE_MISUSE) {
-    fprintf(stderr, "[ERROR]: %s\n", sqlite3_errmsg(db));
-  }
-
-  sqlite3_finalize(pp_stmt);
-
-  return record_set;
-}
-
-json_t *db_query_exec(sqlite3 *db, sqlite3_stmt *pp_stmt,
-                      ResultSetType rs_type) {
-  int rc;
-
-  json_t *record_set = json_array();
-  json_t *record;
-
-  while ((rc = sqlite3_step(pp_stmt)) == SQLITE_ROW) {
-    switch (rs_type) {
-      case TYPE_MOVIE:
-        record = json_pack("{s:s,s:s,s:i,s:i,s:s,s:f,s:s,s:s,s:f}", "title",
-                           sqlite3_column_text(pp_stmt, 1), "genre",
-                           sqlite3_column_text(pp_stmt, 2), "year",
-                           sqlite3_column_int(pp_stmt, 3), "length",
-                           sqlite3_column_int(pp_stmt, 4), "poster_url",
-                           sqlite3_column_text(pp_stmt, 5), "rating_family",
-                           sqlite3_column_double(pp_stmt, 6), "cast",
-                           sqlite3_column_text(pp_stmt, 7), "director",
-                           sqlite3_column_text(pp_stmt, 8), "rating_imdb",
-                           sqlite3_column_double(pp_stmt, 9));
-        if (!record || json_array_append_new(record_set, record)) continue;
-        break;
-      default:
-        assert(0 && "UNIMPLEMENTED");
-    }
   }
 
   if (rc == SQLITE_ERROR || rc == SQLITE_MISUSE) {
