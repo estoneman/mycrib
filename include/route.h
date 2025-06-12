aa3ef09482e041409284d1cc83b6c5e91b8b1ad9
Ethan Stoneman <elstoneman11@gmail.com>
2025-06-11 20:49:52 -0500

    refactor

--- include/route.h
+++ include/route.h
@@ -4,11 +4,23 @@
 #include <jansson.h>
 #include <microhttpd.h>
 
+typedef struct {
+  struct MHD_Connection *connection;
+  const char *url;
+  const char *method;
+  const char *upload_data;
+  const size_t *upload_data_size;
+} RequestContext;
+
 typedef union {
-  json_t *(*handler_arg)(void *req_ctx);
+  json_t *(*handler_arg)(const RequestContext *req_ctx);
   json_t *(*handler_void)(void);
 } Handler;
 
+/**
+ * HANDLER_VOID: A handler that takes no arguments
+ * HANDLER_ARG: A handler that takes arguments
+ */
 typedef enum { HANDLER_ARG, HANDLER_VOID } HandlerType;
 
 typedef struct {
@@ -17,17 +29,31 @@ typedef struct {
   Handler handler;
 } Route;
 
-typedef struct {
-  struct MHD_Connection *connection;
-  const char *method;
-  const char *upload_data;
-  const size_t *upload_data_size;
-} RequestContext;
-
-uint16_t small_crc16_8005(const char *m, size_t n);
+/**
+ * Create a new route and assign various properties to it
+ *
+ * @param routes pointer to currently configured routes
+ * @param path which URL should be handled
+ * @param type type of handler (one of: HANDLER_ARG, HANDLER_VOID)
+ */
 void new_route(Route *routes, const char *path, HandlerType type,
                Handler handler);
+
+/**
+ * Delete a route for a given path
+ *
+ * @param routes currently configured routes
+ * @param path which URL should be deleted
+ */
 void del_route(Route *routes, const char *path);
-enum MHD_Result router(Route *routes, const char *url, RequestContext *req_ctx);
+
+/**
+ * Route requests to the proper handler
+ *
+ * @param routes currently configured routes
+ * @param req_ctx context for the handler to use (e.g., method, url, etc.)
+ * @return result of the handler's operation
+ */
+enum MHD_Result router(Route *routes, const RequestContext *req_ctx);
 
 #endif  // _ROUTE_H
