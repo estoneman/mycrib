2df8e6cfa89cadab0941b7a8578be5a8f87d3f5e
Ethan Stoneman <elstoneman11@gmail.com>
2025-06-11 20:49:11 -0500

    add doc

--- include/util/mhd.h
+++ include/util/mhd.h
@@ -3,14 +3,44 @@
 
 #include <microhttpd.h>
 
+/**
+ * Check whether request method should be allowed
+ *
+ * @param method HTTP method to check
+ * @return MHD_YES when the request is allowed, MHD_NO when not
+ */
 enum MHD_Result dh_check_method(const char *method);
+
+/**
+ * Memory deallocator to use for `send_response_free_callback`
+ *
+ * @param mem pointer to free
+ */
 void mem_free(void *mem);
+
+/**
+ * Wrapper to microhttpd's `MHD_create_response_from_buffer`
+ *
+ * @param connection see microhttpd doc
+ * @param buf see microhttpd doc
+ * @param bufsize see microhttpd doc
+ * @param code see microhttpd doc
+ */
+enum MHD_Result send_response(struct MHD_Connection *connection,
+                              const char *buf, size_t bufsize, int code);
+
+/**
+ * Wrapper to microhttpd's `MHD_create_response_from_buffer_with_free_callback`
+ *
+ * @param connection see microhttpd doc
+ * @param buf see microhttpd doc
+ * @param bufsize see microhttpd doc
+ * @param code see microhttpd doc
+ * @param crfc see microhttpd doc
+ */
 enum MHD_Result send_response_free_callback(struct MHD_Connection *connection,
                                             const char *buf, size_t bufsize,
                                             int code,
                                             MHD_ContentReaderFreeCallback crfc);
-
-enum MHD_Result send_response(struct MHD_Connection *connection,
-                              const char *buf, size_t bufsize, int code);
 
 #endif  // MHD_H_
--- include/util/util.h
+++ include/util/util.h
@@ -4,8 +4,29 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+/**
+ * Get the size of a file
+ *
+ * @param filename name of file of which to calculate its size
+ * @return file size as long
+ */
 long get_file_size(const char *filename);
+
+/**
+ * Load a file in memory
+ *
+ * @param filename name of file to load into memory
+ * @param pointer to file contents
+ */
 char *load_file(const char *filename);
+
+/**
+ * Calculate message digest of a string `m` of length `n` characters
+ *
+ * @param m string of which to calculate message digest
+ * @param n length of `m`
+ * @return 16-bit unsigned hash of `m`
+ */
 uint16_t small_crc16_8005(const char *m, size_t n);
 
 #endif  // _UTIL_H
