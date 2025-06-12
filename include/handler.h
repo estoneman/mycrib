a352ac5da37cdc7d720e01f80de98fcc70697b2e
Ethan Stoneman <elstoneman11@gmail.com>
2025-06-11 20:49:31 -0500

    refactor

--- include/handler.h
+++ include/handler.h
@@ -2,59 +2,22 @@
 #define _HANDLER_H
 
 #include <jansson.h>
-#include <microhttpd.h>
 
+#include "route.h"
+
+/**
+ * Handle requests to '/'
+ *
+ * @return JSON-formatted response
+ */
 json_t *root_handler(void);
 
 /**
- * Returns records of movies that match the given search pattern and type
+ * Handle requests to '/movies'
  *
- * @param connection object that stores data to manipulate the sql query
- * @return JSON-formatted response, regardless if the operation succeeded or not
- *
- * GET
- * /movies?search_type=<exact,startswith,endswith,contains>,search_pattern=<pattern>
- *
- * {
- *     "status": 200,
- *     "result": [
- *       {
- *         "cast"         : "cast",
- *         "director"     : "director",
- *         "duration"     : "duration",
- *         "genre"        : "genre",
- *         "poster_url"   : "poster_url",
- *         "rating_family": "rating_family",
- *         "rating_imdb"  : "rating_imdb",
- *         "title":       : "title",
- *         "year"         : "year"
- *       },
- *       ...
- *     ]
- *  }
- *
- *  {
- *     "status": 400
- *     "result": {
- *       "error": "title not provided"
- *     }
- *  }
- *
- *  {
- *     "status": 404,
- *     "result": {
- *       "error": "%s not found"
- *     }
- *  }
- *
- *  {
- *    "status": 500,
- *    "result": {
- *      "error": "<error message>"
- *    }
+ * @param ctx context to use in order to react differently based on request data
+ * @return JSON-formatted response
  */
-json_t *movies_handler(void *ctx);
-
-json_t *movies_get_handler(struct MHD_Connection *connection);
+json_t *movies_handler(const RequestContext *ctx);
 
 #endif  // _HANDLER_H
