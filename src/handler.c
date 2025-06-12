37fb5a8cd9cb6a307702972320b20b8187971187
Ethan Stoneman <elstoneman11@gmail.com>
2025-06-11 20:50:29 -0500

    refactor

--- src/handler.c
+++ src/handler.c
@@ -4,7 +4,6 @@
 #include <jansson.h>
 #include <microhttpd.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include "db.h"
@@ -12,7 +11,6 @@
 
 #define JSON_ERROR_FMT "{s:i,s:s}"
 #define JSON_ROOT_FMT JSON_ERROR_FMT
-#define JSON_RESULT_FMT "{s:i,s:O}"
 
 json_t *root_handler(void) {
   json_t *result;
@@ -23,152 +21,33 @@ json_t *root_handler(void) {
   return result;
 }
 
-json_t *movies_handler(void *req_ctx) {
-  RequestContext *ctx = req_ctx;
+json_t *movies_handler(const RequestContext *ctx) {
+  json_t *rows;
 
   if (strncmp(ctx->method, MHD_HTTP_METHOD_GET, strlen(MHD_HTTP_METHOD_GET)) ==
       0) {
-    return movies_get_handler(ctx->connection);
+    const char *search_type, *search_pattern;
+
+    search_type = MHD_lookup_connection_value(
+        ctx->connection, MHD_GET_ARGUMENT_KIND, "search_type");
+
+    if (!search_type) search_type = "contains";
+
+    search_pattern = MHD_lookup_connection_value(
+        ctx->connection, MHD_GET_ARGUMENT_KIND, "search_pattern");
+
+    if (!search_pattern)
+      return json_pack(JSON_ERROR_FMT, "status", MHD_HTTP_BAD_REQUEST, "error",
+                       "A search pattern must be provided");
+
+    rows = db_read_movies(search_pattern, search_type);
+    if (!rows)
+      return json_pack(JSON_ERROR_FMT, "status", MHD_HTTP_INTERNAL_SERVER_ERROR,
+                       "error", "Internal Server Error");
+
+    return rows;
   } else {
-    assert(0 && "UNIMPLEMENTED");
+    fprintf(stderr, "Unimplemented handler for HTTP %s\n", ctx->method);
+    assert(0);
   }
-}
-
-json_t *movies_get_handler(struct MHD_Connection *connection) {
-  const char *sql, *search_type, *search_pattern;
-  char *search_pattern_like;
-  int rc;
-  size_t len_search_pattern, len_search_pattern_like;
-  sqlite3 *db;
-  sqlite3_stmt *pp_stmt;
-
-  json_t *record_set, *result, *err;
-
-  search_type = MHD_lookup_connection_value(connection, MHD_GET_ARGUMENT_KIND,
-                                            "search_type");
-
-  if (!search_type) {
-    search_type = "contains";
-  }
-
-  search_pattern = MHD_lookup_connection_value(
-      connection, MHD_GET_ARGUMENT_KIND, "search_pattern");
-
-  if (!search_pattern) {
-    err = json_pack(JSON_ERROR_FMT, "status", MHD_HTTP_BAD_REQUEST, "error",
-                    "A search pattern must be provided");
-
-    return err;
-  }
-
-  if (!(db = db_connect())) {
-    err = json_pack(JSON_ERROR_FMT, "status", MHD_HTTP_INTERNAL_SERVER_ERROR,
-                    "error", "Internal Server Error");
-
-    return err;
-  }
-
-  len_search_pattern = strlen(search_pattern);
-
-  if (strncmp(search_type, "contains", strlen("contains")) == 0) {
-    len_search_pattern_like = len_search_pattern + 2;  // 2 '%'
-
-    if (!(search_pattern_like = malloc(len_search_pattern_like + 1))) {
-      err = json_pack(JSON_ERROR_FMT, "status", MHD_HTTP_INTERNAL_SERVER_ERROR,
-                      "error", "Internal Server Error");
-
-      return err;
-    }
-
-    snprintf(search_pattern_like, len_search_pattern_like + 1, "%%%s%%",
-             search_pattern);
-
-    sql = "SELECT * FROM movies WHERE title LIKE ?;";
-    if (build_stmt_va(&pp_stmt, db, sql, 1, search_pattern_like) == -1) {
-      err = json_pack(JSON_ERROR_FMT, "status", MHD_HTTP_INTERNAL_SERVER_ERROR,
-                      "error", "Internal Server Error");
-
-      return err;
-    }
-
-    free(search_pattern_like);
-  } else if (strncmp(search_type, "endswith", strlen("endswith")) == 0) {
-    len_search_pattern_like = len_search_pattern + 1;  // 1 '%'
-
-    if (!(search_pattern_like = malloc(len_search_pattern_like + 1))) {
-      err = json_pack(JSON_ERROR_FMT, "status", MHD_HTTP_INTERNAL_SERVER_ERROR,
-                      "error", "Internal Server Error");
-
-      return err;
-    }
-
-    snprintf(search_pattern_like, len_search_pattern_like + 1, "%%%s",
-             search_pattern);
-
-    sql = "SELECT * FROM movies WHERE title LIKE ?;";
-    if (build_stmt_va(&pp_stmt, db, sql, 1, search_pattern_like) == -1) {
-      err = json_pack(JSON_ERROR_FMT, "status", MHD_HTTP_INTERNAL_SERVER_ERROR,
-                      "error", "Internal Server Error");
-
-      return err;
-    }
-
-    free(search_pattern_like);
-  } else if (strncmp(search_type, "startswith", strlen("startswith")) == 0) {
-    len_search_pattern_like = len_search_pattern + 1;  // 1 '%'
-
-    if (!(search_pattern_like = malloc(len_search_pattern_like + 1))) {
-      err = json_pack(JSON_ERROR_FMT, "status", MHD_HTTP_INTERNAL_SERVER_ERROR,
-                      "error", "Internal Server Error");
-
-      return err;
-    }
-
-    snprintf(search_pattern_like, len_search_pattern_like + 1, "%s%%",
-             search_pattern);
-
-    sql = "SELECT * FROM movies WHERE title LIKE ?;";
-    if (build_stmt_va(&pp_stmt, db, sql, 1, search_pattern_like) == -1) {
-      err = json_pack(JSON_ERROR_FMT, "status", MHD_HTTP_INTERNAL_SERVER_ERROR,
-                      "error", "Internal Server Error");
-
-      return err;
-    }
-
-    free(search_pattern_like);
-  } else if (strncmp(search_type, "exact", strlen("exact")) == 0) {
-    sql = "SELECT * FROM movies WHERE title = ?;";
-    if (build_stmt_va(&pp_stmt, db, sql, 1, search_pattern) == -1) {
-      err = json_pack(JSON_ERROR_FMT, "status", MHD_HTTP_INTERNAL_SERVER_ERROR,
-                      "error", "Internal Server Error");
-
-      return err;
-    }
-  } else {
-    err = json_pack(JSON_ERROR_FMT, "status", MHD_HTTP_BAD_REQUEST, "error",
-                    "Unsupported search type");
-    return err;
-  }
-
-  record_set = db_exec_movie(db, pp_stmt);
-
-  if (!record_set) {
-    err = json_pack(JSON_ERROR_FMT, "status", MHD_HTTP_INTERNAL_SERVER_ERROR,
-                    "error", "Internal Server Error");
-
-    return err;
-  }
-
-  result =
-      json_pack(JSON_RESULT_FMT, "status", MHD_HTTP_OK, "result", record_set);
-  json_decref(record_set);
-
-  if ((rc = db_close(db)) != 0) {
-    err = json_pack(JSON_ERROR_FMT, "status", MHD_HTTP_INTERNAL_SERVER_ERROR,
-                    "error", "Internal Server Error");
-
-    return err;
-  }
-
-  return result;
 }
