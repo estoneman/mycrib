c35d290daeb9b7d7f963a97774807ce3627f6a66
Ethan Stoneman <elstoneman11@gmail.com>
2025-06-13 14:58:20 -0500

    refactor

--- include/handler.h
+++ include/handler.h
@@ -3,21 +3,39 @@
 
 #include <jansson.h>
 
-#include "route.h"
+typedef struct {
+  struct MHD_Connection *connection;
+  const char *url;
+  const char *method;
+  const char *upload_data;
+  const size_t *upload_data_size;
+} RequestContext;
+
+typedef json_t *(*Handler)(const RequestContext *req_ctx);
 
 /**
  * Handle requests to '/'
  *
+ * @param req_ctx context to use in order to react differently based on request
+ * data
  * @return JSON-formatted response
  */
-json_t *root_handler(void);
+json_t *root_handler(const RequestContext *req_ctx);
 
 /**
  * Handle requests to '/movies'
  *
- * @param ctx context to use in order to react differently based on request data
+ * @param req_ctx context to use in order to react differently based on request
+ * data
  * @return JSON-formatted response
+ *
+ * This handler expects the following query string parameters
+ *   - search_type: type of search (contains[default], exact, startswith,
+ *     endswith)
+ *   - search_pattern: string to search for
+ *   - search_by: column to search by (e.g., title, cast, release_date, etc.)
+ *     see database schema for available fields
  */
-json_t *movies_handler(const RequestContext *ctx);
+json_t *movies_handler(const RequestContext *req_ctx);
 
 #endif  // _HANDLER_H
