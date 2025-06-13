4be6464e4435eba774260b442a4da2e47b8eee00
Ethan Stoneman <elstoneman11@gmail.com>
2025-06-13 14:58:08 -0500

    refactor, remove HandlerType

--- include/route.h
+++ include/route.h
@@ -4,28 +4,10 @@
 #include <jansson.h>
 #include <microhttpd.h>
 
-typedef struct {
-  struct MHD_Connection *connection;
-  const char *url;
-  const char *method;
-  const char *upload_data;
-  const size_t *upload_data_size;
-} RequestContext;
-
-typedef union {
-  json_t *(*handler_arg)(const RequestContext *req_ctx);
-  json_t *(*handler_void)(void);
-} Handler;
-
-/**
- * HANDLER_VOID: A handler that takes no arguments
- * HANDLER_ARG: A handler that takes arguments
- */
-typedef enum { HANDLER_ARG, HANDLER_VOID } HandlerType;
+#include "handler.h"
 
 typedef struct {
   char *path;
-  HandlerType type;
   Handler handler;
 } Route;
 
@@ -36,8 +18,7 @@ typedef struct {
  * @param path which URL should be handled
  * @param type type of handler (one of: HANDLER_ARG, HANDLER_VOID)
  */
-void new_route(Route *routes, const char *path, HandlerType type,
-               Handler handler);
+void new_route(Route *routes, const char *path, Handler handler);
 
 /**
  * Delete a route for a given path
