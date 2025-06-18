22707245ea7b650c7bd62e418104238bb035536a
estoneman <elstoneman11@gmail.com>
2025-06-17 22:10:42 -0500

    fix cert gen

--- scripts/mk_cert.sh
+++ scripts/mk_cert.sh
@@ -21,20 +21,28 @@ _gen_ss_cert ()
     # $1 => number of days
 
     printf 'Generating self-signed certificate... '
-    openssl req -days "$1" -out mycrib.pem -new -x509 -key "$_OUT/mycrib.key"
+    openssl req -days "$1" -out "$_OUT/mycrib.pem" -new -x509 -key "$_OUT/mycrib.key"
     echo 'Done'
 }
+
+if ! [ -d "$_OUT" ]; then
+    mkdir -p "$_OUT"
+fi
 
 _BITS=4096
 if [ -f "$_OUT/mycrib.key" ]; then
     read -p 'Key exists, overwrite (y/n)? ' -r -n 1 _PERM; echo
     [ "${_PERM@L}" = 'y' ] && _gen_key $_BITS
+else
+    _gen_key $_BITS
 fi
 
 _DAYS=365
 if [ -f "$_OUT/mycrib.pem" ]; then
     read -p 'Certificate exists, overwrite (y/n)? ' -r -n 1 _PERM; echo
     [ "${_PERM@L}" = 'y' ] && _gen_ss_cert $_DAYS
+else
+    _gen_ss_cert $_DAYS
 fi
 
 unset _BITS _OUT _PERM _PROJECT_HOME
